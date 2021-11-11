ae4ccd92b15f76cf7f5e5202dbb013811f7ef008
Denis Mathan <denis.mathan@googlemail.com>
2021-11-11 13:03:10 +0100

    initial commit

---
+++ src/js/threeD.js
@@ -0,0 +1,108 @@
+import * as THREE from 'three';
+import {Text} from 'troika-three-text';
+let scene, camera, renderer, group;
+let radius;
+let maxSpeed = 0.02;
+let position = {
+    x: 0,
+    y: 0,
+};
+let br = false;
+
+let init = (box)=>{
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, box.offsetWidth / box.offsetHeight, 0.1, 1000 );
+    renderer = new THREE.WebGLRenderer();
+    radius = box.offsetWidth/29.2;
+    renderer.setPixelRatio(4);
+    renderer.setSize(box.offsetWidth, box.offsetHeight)
+    console.log(box.offsetWidth)
+    box.appendChild(renderer.domElement);
+    camera.position.z = 20;
+    camera.position.y = 0; 
+    box.addEventListener('mousemove', direction);
+    box.addEventListener('mouseout', breaky);
+    group = new THREE.Object3D();
+    scene.add(group);
+    // addSphere();
+    addText('HTML', 0, -radius, 0);
+    addText('JS', -radius/1.4142, -radius/1.4142, 0);
+    addText('Java', radius/1.4142, radius/1.4142, 0);
+    addText('illustrator', radius/1.4142, -radius/1.4142, 0);
+    addText('3Ds Max', -radius/1.4142, radius/1.4142, 0);
+    addText('git', 0, radius/1.4142, -radius/1.4142,);
+    addText('Threejs', 0, -radius/1.4142, radius/1.4142,);
+    addText('vuejs', 0, -radius/1.4142, -radius/1.4142,);
+    addText('firebase', 0, radius/1.4142, radius/1.4142,);
+    addText('wordpress', radius/1.4142, 0,-radius/1.4142);
+    addText('Photoshop', -radius/1.4142,0, radius/1.4142);
+    addText('Python', -radius/1.4142, 0,-radius/1.4142);
+    addText('ableton', radius/1.4142,0, radius/1.4142);
+    addText('react',radius, 0, 0);
+    addText('gulp',-radius, 0, 0);
+    addText('nunjucks', 0, radius, 0);
+    addText('tailwind.css',0, 0, radius);
+    addText('react',0, 0, -radius);
+    
+    group.rotateOnAxis(new THREE.Vector3(1,0,0), 0.45)
+    animate();
+}
+let animate = ()=>{
+    let x = position.x;
+    let y = position.y;
+    let axis = new THREE.Vector3(y,x,0)
+
+
+    if (br){
+        maxSpeed *= 0.95;
+    }
+    group.rotateOnWorldAxis(axis,maxSpeed)
+    for (let i=0; i<group.children.length; i++){
+        const element = group.children[i];
+        element.rotateOnAxis(axis, -maxSpeed);
+    }
+    renderer.render(scene, camera)
+    requestAnimationFrame( animate );
+    
+
+}
+let addText = (word, x, y, z)=> {
+    const text = new Text();
+    text.text = word;
+    text.fontSize = 1;
+    text.color = 0xFFFFFF;
+    text.anchorX = 'center'
+    text.anchorY = 'middle';
+    text.position.x= x;
+    text.position.y = y;
+    text.position.z = z;
+    text.sync();
+    text.rotateOnAxis(new THREE.Vector3(1,0,0), -0.45)
+    group.add(text)
+    renderer.render(scene, camera)
+}
+let breaky = ()=>{
+    br = true;
+    
+}
+let direction = (e)=>{
+    let norm
+    position.x = -1 + e.offsetX/(e.target.offsetWidth/2);
+    position.y = -1 + e.offsetY/(e.target.offsetHeight/2);
+    // position.distance = Math.sqrt(sqr(position.x,2) + sqr(position.y,2))
+    // norm = Math.sqrt(sqr(position.x,2)+sqr(position.y,2))
+    // position.x = position.x/norm;
+    // position.y = position.y/norm;
+    if(br){
+        br = false;
+        maxSpeed = 0.02;
+    }
+}
+let sqr = (x,pot)=>{
+    let val = x;
+    for(let i = 2; i<=pot; i++){
+      val *= x;
+    }
+    return val;
+  }
+export {init}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello</h1>
+    <!-- <div ref="gl" class=threeDContainer></div> -->
+<!--     <div class="canvasContainer">
+        <canvas width="250" height="250" ref="myCanvas">
+        </canvas>
+        
+    </div> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+// import {init} from '@/js/threeD.js';
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  data(){return{
+    canvas: null,
+    words: [{
+      name: 'HTML',
+      xpos: 0,
+      ypos: 0,
+      pos: 1
+    }],
+    three:{
+      scene: null,
+      camera: null,
+      renderer: null,
+      cube: null
+    },
+    renderer: null
+  }},
+  mounted(){
+    // init(this.$refs.gl);
+  },
+  methods:{
+
+  } 
 }
 </script>
+<style scoped>
+  .threeDContainer{
+    width: 50vw;
+    height:50vw;
+  }
+</style>
