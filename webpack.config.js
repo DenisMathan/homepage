909ff0353fff0844e91d113b7a971af01be27472
Denis Mathan <denis.mathan@googlemail.com>
2021-11-12 20:50:22 +0100

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+/* const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 plugins: [
   new BundleAnalyzerPlugin()
   ]
+ */
