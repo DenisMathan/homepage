c3de344a0cac0c21ef53729fc2b482791ba37de3
Denis Mathan <denis.mathan@googlemail.com>
2021-11-12 20:21:55 +0100

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Hello</h1>
-    <!-- <div ref="gl" class=threeDContainer></div> -->
+    <div ref="gl" class=threeDContainer></div>y
 <!--     <div class="canvasContainer">
         <canvas width="250" height="250" ref="myCanvas">
         </canvas>
@@ -13,7 +13,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-// import {init} from '@/js/threeD.js';
+import {init} from '@/js/threeD.js';
 export default {
   name: 'Home',
   components: {
@@ -36,7 +36,7 @@ export default {
     renderer: null
   }},
   mounted(){
-    // init(this.$refs.gl);
+    init(this.$refs.gl);
   },
   methods:{
 
