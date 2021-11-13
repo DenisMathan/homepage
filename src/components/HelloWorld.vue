e74ec948c1dfe46dc601a78846eeee503015bb71
Denis Mathan <denis.mathan@googlemail.com>
2021-11-13 14:17:07 +0100

    added kugel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div ref="gl" class=threeDContainer></div>
     <p>
      Testing
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -26,14 +27,19 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
   </div>
 </template>
 
 <script>
+import {init} from '@/js/threeD.js';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted(){
+    init(this.$refs.gl)
   }
 }
 </script>
@@ -54,4 +60,8 @@ li {
 a {
   color: #42b983;
 }
+  .threeDContainer{
+    width: 50vw;
+    height:50vw;
+  }
 </style>
