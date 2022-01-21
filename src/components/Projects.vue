5ff6e0278b9b47d515e63aca9876d7c9436c8962
Denis Mathan <denis.mathan@googlemail.com>
2022-01-21 22:20:38 +0100

    changed all images to webp

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -21,7 +21,7 @@ export default {
       programming: [
         {
           name: "Bachelor-Thesis",
-          img: require('@/assets/images/ba/spur.png'),
+          img: require('@/assets/images/ba/spur.webp'),
           description: [
             'Conception & realization of a progressive web app to support musicians in their songwriting process by means of developing of a digital audio workstation and a lead sheet creation tool.',
             ],
@@ -31,7 +31,7 @@ export default {
         },
         {
           name: "Synthesizer",
-          img: require('@/assets/images/synthesizer.png'),
+          img: require('@/assets/images/synthesizer.webp'),
           description: [
             'I programmed the synthesizer during the fourth semester for fun, because I wanted to get a bit more into React and Typescript. It\'s very rudimentary and the sound isn\'t the best either, but at least you can play simple ditties with it.',
             'The basic code I was able to work my way into was given to me at the time by my lecturers from the subject "Internet Applications". Unfortunately, I could only find the finished build of what I made out of it. For this reason, I can only present the result and not my code. The code in my Github, is the one that the lecturers had given me at that time.'
@@ -44,7 +44,7 @@ export default {
           name: "Rocket-Simulation",
           text: 'dark',
           link: 'https://codepen.io/Gnomath/pen/gOaeYzR',
-          img: require('@/assets/images/rocketShip.png'),
+          img: require('@/assets/images/rocketShip.webp'),
           description:['To be honest this has nothing to do with a simulation. It was just a little afternoon project to get to know vue.js a little better. It was a weekly assignment from codepen. I can\'t remember its exact wording. If you want to take a closer look, check out my codepen.'],
           size: 'middle',
           position: undefined
@@ -54,7 +54,7 @@ export default {
           name: "Song-Writing-App 0.1",
           text: 'dark',
           learnMore: 'firstApp',
-          img: require('@/assets/images/songwritingApp_1_0/recordwindow.png'),
+          img: require('@/assets/images/songwritingApp_1_0/recordwindow.webp'),
           description: [
             'The songwriting app began as an ambitious project in the fourth semester. At the time, it was only supposed to be able to capture various ideas both lyrically and acoustically. The creation of this program required two intensive weeks in which I had created a pretty decent program for a beginner. The most difficult part for me at the time was the recording, although the features here don\'t begin to compare to those of the bachelor thesis I tackled a year later.'
           ],
@@ -63,7 +63,7 @@ export default {
         },
         {
           name: "Internship at coma",
-          img: require('@/assets/images/coma.png'),
+          img: require('@/assets/images/coma.webp'),
           learnMore: 'coma',
           goto: "https://www.coma.de/",
           description: ["Coma was not a project in that sense, but it was my first experience in the IT working world, away from university projects. I did my five-month internship for my studies at this company in 2020."],
@@ -72,7 +72,7 @@ export default {
         },
         {
           name: 'Hemp-for-future',
-          img: require('@/assets/images/hemp-for-future.png'),
+          img: require('@/assets/images/hemp-for-future.webp'),
           goto: 'https://hemp-for-future.web.app/',
           github: 'https://github.com/DenisMathan/hemp_for_future',
           description:[
@@ -83,7 +83,7 @@ export default {
         },
         {
           name: "Chess",
-          img: require('@/assets/images/Schach/1start.png'),
+          img: require('@/assets/images/Schach/1start.webp'),
           learnMore: 'chess',
           text: 'dark',
           description:[
@@ -97,7 +97,7 @@ export default {
         {
           name: "Medipus",
           position: 1,
-          img: require('@/assets/images/medipusI.png'),
+          img: require('@/assets/images/medipusI.webp'),
           vid: require('@/assets/videos/Der_Medipus.mp4'),
           description: [
             '“Der Medipus" is about an octopus that could be said to have a somewhat unhealthy media consumption. Thanks to his eight arms, each of which is equipped with a brain, he also uses many technical devices at the same time. Whether this will be his undoing, you can find out here.',
@@ -108,7 +108,7 @@ export default {
           name: "Pirate",
           position: 1,
           vid: require('@/assets/videos/walkcycle.mp4'),
-          img: require('@/assets/videos/walkcycle.png'),
+          img: require('@/assets/videos/walkcycle.webp'),
           description:[
               'This was a simple task that everyone had to do individually before the stop motion movie. The task was to animate this running pirate. Everyone was free to design the environment.',  
               'The program used was After Effects with the Duik Bassel plugin. However, at that time it was still very buggy, which led to some nervous breakdowns. Fortunately, it worked pretty well for me.'
@@ -117,7 +117,7 @@ export default {
         {
           name: "Zwischen Familie, Arbeit & Rock'n Roll",
           position: 2,
-          img: require('@/assets/images/simonPoster.png'),
+          img: require('@/assets/images/simonPoster.webp'),
           vid: require('@/assets/videos/Simon.mp4'),
           description:[
             'In the 2nd semester of my studies, I had to shoot a portrait. Our film is about Simon, an enthusiastic musician who, in addition to music, also has to reconcile work and his family. He has been an enthusiastic musician since his youth and has been living out this lifestyle for years.  But now, with his little daughter, another big responsibility has come along.',
@@ -128,7 +128,7 @@ export default {
         {
           name: "Turbina",
           position: 3,
-          img: require('@/assets/images/autoPoster.png'),
+          img: require('@/assets/images/autoPoster.webp'),
           vid: require('@/assets/videos/TurbinaFilm.mp4'),
           description:[
             'This film had to be animated by everyone in the subject of animation using the program 3D\'s Max. The only requirement was to recreate the body of the Fiat Turbina from 1954. The environment in which this was then allowed to go into action was left up to everyone.'
